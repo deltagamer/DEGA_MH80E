65b70891e6f2a8d5a2f526d7396cff5623fb2ec6
deltagamer <alexstrathearn97@gmail.com>
2023-04-02 13:26:31 +0100

    Add files via upload

---
+++ addons/dega_vehicles_MH80E/data/release/mod.cpp
@@ -0,0 +1,15 @@
+
+name = "DEGA: MH80E Shadow Hawk";
+description = "DEGA: MH80E Shadow Hawk Modification";
+picture = "\dega_vehicles_MH80E\data\release\logo_dega.paa";
+actionName = "";
+action = "";
+
+logo = "\dega_vehicles_MH80E\data\release\logo_dega.paa";
+logoOver = "\dega_vehicles_MH80E\data\release\logo_dega.paa";
+logoSmall = "\dega_vehicles_MH80E\data\release\logo_dega_small.paa";
+
+tooltip = "Deltagamer: MH80E Shadow Hawk";
+tooltipOwned = "Deltagamer: MH80E Shadow Hawk - Owned";
+overview = "MH80E Shadow Hawk: Adds custom vanilla ghosthawk edit";
+author = "Deltagamer";
